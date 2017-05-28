5c9ef654e1cec5fed83d8c9087cbe58dfb800ebd
Javier Barrera <javier.barrera@starloopstudios.com>
2017-05-28 11:54:49 +0200

    - Bot Framework template - First modifications in RootDialog's MessageReceivedAsync method

---
+++ Kora/Dialogs/RootDialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Kora.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            string text = activity.Text;
+            string answer = string.Empty;
+
+            if (text.StartsWith("/add"))
+            {
+
+            }
+            else if (text.StartsWith("/list"))
+            {
+
+            }
+            else if (text.StartsWith("/complete"))
+            {
+
+            }
+
+            // return our reply to the user
+            await context.PostAsync(answer);
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Kora/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kora.WebApiApplication" Language="C#" %>
